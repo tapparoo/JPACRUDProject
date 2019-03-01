d62f19cac26b9b88c86ff73be29381dc13c92adc
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-03-01 15:44:02 -0700

    Renamed entity packages

---
+++ BootMVCSnakes/src/main/java/com/skilldistillery/snakes/BootMvcSnakesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.snakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcSnakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcSnakesApplication.class, args);
+	}
+
+}
