0d7970877a781bcfe124bcf60cf08237876aef4e
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2019-03-04 08:50:40 -0700

    Prep for server deployment

--- BootMVCSnakes/src/main/java/com/skilldistillery/bootmvcsnakes/BootMvcSnakesApplication.java
+++ BootMVCSnakes/src/main/java/com/skilldistillery/bootmvcsnakes/BootMvcSnakesApplication.java
@@ -3,10 +3,16 @@ package com.skilldistillery.bootmvcsnakes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EntityScan("com.skilldistillery.jpasnakes")
 @SpringBootApplication
-public class BootMvcSnakesApplication {
+public class BootMvcSnakesApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootMvcSnakesApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMvcSnakesApplication.class, args);
